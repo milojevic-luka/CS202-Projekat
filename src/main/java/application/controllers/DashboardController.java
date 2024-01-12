8f6465b4b4776ee310252ddade7fbb11835bbb41
Luka <luka.milojevic.5673@metropolitan.ac.rs>
2024-01-12 22:05:17 +0100

    Created method for populating Dashboard

--- src/main/java/application/controllers/DashboardController.java
+++ src/main/java/application/controllers/DashboardController.java
@@ -1,16 +1,21 @@
 package application.controllers;
 
+import application.db.DatabaseConnection;
 import application.ui.ConfirmAlert;
 import application.ui.InfoAlert;
 import application.ui.SwitchScene;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 import java.io.IOException;
+import java.net.URL;
+import java.sql.*;
+import java.util.ResourceBundle;
 
-public class DashboardController {
+public class DashboardController implements Initializable {
     @FXML
     private Button coachesButton;
 
@@ -42,12 +47,12 @@ public class DashboardController {
     void logOut(ActionEvent event) throws IOException {
         boolean isConfirmed = ConfirmAlert.show("Confirmation message",
                 "Are you sure you want to log out?");
-        if(isConfirmed) SwitchScene.change("Log in", "main-view.fxml", event);
+        if (isConfirmed) SwitchScene.change("Log in", "main-view.fxml", event);
     }
 
     @FXML
-    void switchToCoaches(ActionEvent event) {
-
+    void switchToCoaches(ActionEvent event) throws IOException {
+        SwitchScene.change("Coaches", "coach-view.fxml", event);
     }
 
     @FXML
@@ -63,5 +68,36 @@ public class DashboardController {
     @FXML
     void switchToSupplements(ActionEvent event) {
 
+    }
+
+    private void getDashboardData() {
+        try (Connection connection = new DatabaseConnection().connectToDb()) {
+            String sql = "SELECT " +
+                    "(SELECT COUNT(*) FROM MEMBER) AS member_count, " +
+                    "(SELECT COUNT(*) FROM COACH) AS coach_count, " +
+                    "(SELECT COUNT(*) FROM MEMBERSHIP) AS membership_count, " +
+                    "(SELECT SUM(PRICE) FROM MEMBERSHIP) AS revenue";
+            Statement statement = connection.createStatement();
+            ResultSet result = statement.executeQuery(sql);
+            if (result.next()) {
+                int memberCount = result.getInt("member_count");
+                int coachCount = result.getInt("coach_count");
+                int membershipCount = result.getInt("membership_count");
+                double totalRevenue = result.getDouble("revenue");
+
+                totalActiveMembershipsLabel.setText(totalActiveMembershipsLabel.getText() + membershipCount);
+                totalCoachesLabel.setText(totalCoachesLabel.getText() + coachCount);
+                totalMembersLabel.setText(totalMembersLabel.getText() + memberCount);
+                totalRevenuLabel.setText(totalRevenuLabel.getText() + totalRevenue + "$");
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        getDashboardData();
     }
 }
