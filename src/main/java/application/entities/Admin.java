ffccd835dbf37e7b9d23cc4a1ca74ba79f68db0a
Luka <luka.milojevic.5673@metropolitan.ac.rs>
2024-01-13 21:18:37 +0100

    Added email for Admin

--- src/main/java/application/entities/Admin.java
+++ src/main/java/application/entities/Admin.java
@@ -4,13 +4,27 @@ public class Admin {
 
     private int id;
     private String username;
+    private String email;
     private String password;
 
     public Admin() {
     }
 
+    public Admin(int id, String username, String email, String password) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
     public Admin(String username, String password) {
         this.username = username;
+        this.password = password;
+    }
+
+    public Admin(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
         this.password = password;
     }
 
@@ -38,11 +52,20 @@ public class Admin {
         this.password = password;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     @Override
     public String toString() {
         return "Admin{" +
                 "id=" + id +
                 ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
                 ", password='" + password + '\'' +
                 '}';
     }
